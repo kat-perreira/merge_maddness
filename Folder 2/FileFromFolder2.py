b7d954db1cb79a6e603624dd8b8d8363945e3226
kat-perreira <kat.perreira@highspot.com>
2021-09-18 16:42:24 -0700

    response to melinda

--- Folder 2/FileFromFolder2.py
+++ Folder 2/FileFromFolder2.py
@@ -8,19 +8,23 @@
 # It is raining in Atlanta
 # What is the weather in Seattle?
 
-#Dear Genie's diary, 
+#Dear Genie's diary,
 # Well same here, raining in Seattle
 #What did you do today?
 
 
 # Dear Genie's Diary,
 
-# It took me 3 hours to get out of bed and go to Target. 
-# But I saw this cute show called Alice in Paris. 
-# And now I want to go to Paris and eat. 
+# It took me 3 hours to get out of bed and go to Target.
+# But I saw this cute show called Alice in Paris.
+# And now I want to go to Paris and eat.
 # What did you do?
 
-#Dear Genie's Diary, 
+#Dear Genie's Diary,
 # Kat and I ate breakfast this morning
 #Then came to her house where I have been trying to understand this Git and Github stuff
 #Then she tried to kill me with her hot sauce
+
+# Dear Genies diary,
+# If by kill you mean bestowed upon you an experience of euphoria where you got to enjoy the absolute joy of eating
+# glorious hot sauce then....you're welcome :)
