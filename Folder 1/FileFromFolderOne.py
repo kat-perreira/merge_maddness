5e308e2033cd199328d9695df321269739f6bbef
Genie <geniearcila@gmail.com>
2021-09-18 19:08:38 -0400

    write a poem and add diary entry

--- Folder 1/FileFromFolderOne.py
+++ Folder 1/FileFromFolderOne.py
@@ -7,3 +7,11 @@
 # I feel like shit
 # because I cant commit
 # and I want to quit.
+
+
+
+# Genie's poem about Git
+
+# This is a haiku
+# About the dreaded Github
+# I hate it so much
--- Folder 2/FileFromFolder2.py
+++ Folder 2/FileFromFolder2.py
@@ -1,3 +1,9 @@
 "Hello, how are you?"
 # hello hows it going im just writing stuff
 # yee yee
+
+# Dear Genie's diary,
+
+# Heeeey everyone!
+# It is raining in Atlanta
+# What is the weather in Seattle?
