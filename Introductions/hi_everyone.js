996bc76de4cd0b0ab652eb27bf4e3a7ec837b0df
kat-perreira <kat.perreira@highspot.com>
2021-09-18 19:11:49 -0700

    fixed intro file

---
+++ Introductions/hi_everyone.js
@@ -0,0 +1,12 @@
+// Hi everyone now im adding in a javascript file in a branch
+
+// I am used to the hash tags
+//
+
+const melinda = "Add something about yourself";
+const genie = "Add something about yourself";
+const lindsey = "Add something about yourself";
+const kat =
+  "doesnt actually understand anything about software develpment or writing code they're faking it";
+
+const peopleObject = { c10: [kat], c15: [lindsey], c16: [melinda, genie] };
